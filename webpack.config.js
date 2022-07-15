0ad4707698718d1df17052be9a8c57238aa02f19
Bartosz Trusinski <bartosztrusinski@gmail.com>
2022-07-15 20:11:30 +0200

    organize config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV || "development",
   devtool: "inline-source-map",
   entry: "./src/index.ts",
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
   },
   module: {
     rules: [
-      {test: /\.tsx?$/, loader: "ts-loader"}
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
     ]
   }
 };
